478a80a6fcc51280d2bd4716be75296139251169
Ehsan Montahaei <ehsan.m.a.e@gmail.com>
2023-10-18 01:29:58 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 rm IranianMarkets@ehsan.zip
 zip -r IranianMarkets@ehsan.zip ./IranianMarkets@ehsan/
-gnome-extensions pack ./IranianMarkets@ehsan/
+# gnome-extensions pack ./IranianMarkets@ehsan/
 gnome-extensions install -f IranianMarkets@ehsan.zip # -f is for overwrite
 gnome-extensions enable IranianMarkets@ehsan
